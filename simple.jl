c113739017592cc7cf8fbd43c624034d09ea822b
Roberto <roberto@debian.admin>
2025-10-13 12:34:44 -0600

    Initial commit

--- simple.jl
+++ simple.jl
@@ -2,10 +2,28 @@ using Agents, Random
 using StaticArrays: SVector
 
 @agent struct Car(ContinuousAgent{2,Float64})
+    accelerating::Bool = true
 end
 
-function agent_step!(agent, model)
-    move_agent!(agent, model, 1.0)
+accelerate(agent) = agent.vel[1] + 0.05
+decelerate(agent) = agent.vel[1] - 0.1
+
+function  agent_step!(agent, model)
+    new_velocity = agent.accelerating ? accelerate(agent) : decelerate(agent)
+
+    if new_velocity >= 1.0
+        new_velocity = 1.0
+        agent.accelerating = false
+    elseif new_velocity <= 0.0
+        new_velocity = 0.0
+        agent.accelerating = true
+    end
+    if agent.id == 1
+        println(agent.pos)
+    end
+    
+    agent.vel = (new_velocity, 0.0)
+    move_agent!(agent, model, 0.4)
 end
 
 function initialize_model(extent = (25, 10))
@@ -14,8 +32,15 @@ function initialize_model(extent = (25, 10))
 
     model = StandardABM(Car, space2d; rng, agent_step!, scheduler = Schedulers.Randomly())
 
+    first = true
+    py = 1.0
     for px in randperm(25)[1:5]
-        add_agent!(SVector{2, Float64}(px, 0.0), model; vel=SVector{2, Float64}(1.0, 0.0))
+        if first
+            add_agent!(SVector{2, Float64}(px, py), model; vel=SVector{2, Float64}(1.0, 0.0))
+        else
+            add_agent!(SVector{2, Float64}(px, py), model; vel=SVector{2, Float64}(rand(Uniform(0.2, 0.7)), 0.0))
+        end
+        py += 2.0
     end
     model
 end
