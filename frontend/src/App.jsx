8f0f467945ba53dcb1a04a17eb4fd4c47c57c211
Sebastian Ponce <poncegramming@gmail.com>
2025-10-22 21:29:17 -0600

    add crossing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,19 @@ import { useState, useRef } from 'react';
 
 export default function App() {
   let [location, setLocation] = useState("");
-  let [cars, setCars] = useState([]);
+  let [lights, setLights] = useState([]);
   let [simSpeed, setSimSpeed] = useState(10);
   const running = useRef(null);
 
   let setup = () => {
-    console.log("Hola");
     fetch("http://localhost:8000/simulations", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({  })
     }).then(resp => resp.json())
     .then(data => {
-      console.log(data);
       setLocation(data["Location"]);
-      setCars(data["cars"]);
+      setLights(data["lights"]);
     });
   }
 
@@ -25,7 +23,7 @@ export default function App() {
       fetch("http://localhost:8000" + location)
       .then(res => res.json())
       .then(data => {
-        setCars(data["cars"]);
+        setLights(data["lights"]);
       });
     }, 1000 / simSpeed);
   };
@@ -34,36 +32,23 @@ export default function App() {
     clearInterval(running.current);
   }
 
-  const handleSimSpeedSliderChange = (event, newValue) => {
-    setSimSpeed(newValue);
-  };
-
-
   return (
     <div>
       <div>
-        <button onClick={setup}>
-          Setup
-        </button>
-        <button onClick={handleStart}>
-          Start
-        </button>
-        <button onClick={handleStop}>
-          Stop
-        </button>
+        <button onClick={setup}>Setup</button>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
       </div>
       <svg width="800" height="500" xmlns="http://www.w3.org/2000/svg" style={{backgroundColor:"white"}}>
-
-      <rect x={0} y={200} width={800} height={80} style={{fill: "darkgray"}}></rect>
-      {/* <image x={0} y={240} href="./racing-car.png"/> */}
-      {
-        cars.map(car =>
-          <image id={car.id} x={car.pos[0]*32} y={240} width={32} href="./racing-car.png"/>
-        )
-      }
+        <rect x={0} y={200} width={800} height={100} fill="darkgray" />
+        <rect x={350} y={0} width={100} height={500} fill="darkgray" />
+        {lights.map(light => {
+          const col = light.color === 0 ? 'green' : light.color === 1 ? 'yellow' : 'red';
+          const cx = light.pos[0] * 80;
+          const cy = light.pos[1] * 50;
+          return <circle key={light.id} cx={cx} cy={cy} r={10} fill={col} stroke="black" />;
+        })}
       </svg>
-
-
     </div>
   );
 }
